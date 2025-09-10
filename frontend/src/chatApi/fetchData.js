a4f80811a47053f6c36ee6d7032541a0d3b255df
JavierQuinan <javierquiandra@gmail.com>
2025-09-09 20:39:12 -0500

    Empezar_0

--- frontend/src/chatApi/fetchData.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/chatApi/fetchData.js
-import api from './api.js';
-
-export default async function fetchData() {
-  // Trae el seed completo: { channels, messages, currentChannelId, username }
-  const { data } = await api.get('/data'); // -> /api/v1/data por tu baseURL
-  return data;
-}
--- frontend/src/locales/index.js
+++
@@ -1,7 +0,0 @@
-import en from './en.js';
-
-const resources = {
-  en,
-};
-
-export default resources;
