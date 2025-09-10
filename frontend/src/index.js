a4afdd96bd98aefd9f1c00af70fb8c939db1582b
JavierQuinan <javierquiandra@gmail.com>
2025-09-09 22:39:48 -0500

    fix: force-add frontend app files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
+// frontend/src/index.js
+// import React from 'react'; // Se elimina si no usamos React directamente
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import init from './init.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+init().then((vdom) => {
+  root.render(vdom);
+});
+
 reportWebVitals();
